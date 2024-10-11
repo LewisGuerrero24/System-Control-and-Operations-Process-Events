6f05ab99336d19676bd32b91052646971aa58596
Lewis Guerrero <lewisguerrero20@gmail.com>
2024-10-10 21:32:29 -0500

    --version 10/10/2024

---
+++ Services/GCP/Api/Program.cs
@@ -0,0 +1,38 @@
+
+using Application.Services;
+using Domain.Ports;
+using Infrastructure.Adapters;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// builder.Services.AddScoped<UserService>();
+// builder.Services.AddScoped<IUserRepository,UserRepository>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// builder.Services.AddDbContext<MyContext>(
+//     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),   
+ 
+//         b => b.MigrationsAssembly("Infrastructure")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers(); // Asegúrate de mapear los controladores
+
+app.Run();
---
+++ Services/GCP/Core/Infrastructure/Data/MyContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class MyContext : DbContext
+    {
+         public MyContext(DbContextOptions<MyContext> options): base(options){
+            
+            }
+
+    }
+}
